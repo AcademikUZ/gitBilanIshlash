becce26e30836fa0eabb331132788e5be6a534af
Akademik <you@example.com>
2022-04-06 10:15:23 +0500

    second-commit

---
+++ gitBilanIshlash/src/main/java/com/example/gitbilanishlash/payload/ModelDto.java
@@ -0,0 +1,19 @@
+package com.example.gitbilanishlash.payload;
+
+import com.example.gitbilanishlash.entity.Hotel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.ManyToOne;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ModelDto {
+    private String number;
+    private String floor;
+    private Double size;
+    private String nameHotel;
+
+}
